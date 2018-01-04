118df22c4cc3b94bc18d838c200c951957a9668e
ev-dev <evan.reed1100@gmail.com>
2018-01-03 23:43:30 -0500

    custom grid setup

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
 
-const SideBar = () => [
-  <nav>
-  
-  </nav>,
-  <footer>
-  
-  </footer>
-]
+const SideBar = () =>
+  <aside>
+    <div id='sidebar-photo-container'></div>
+    <div id='sidebar-menu-container'>
+      <div className='sidebar-menu-item'>
+        Option
+      </div>
+      <div className='sidebar-menu-item'>
+        Option
+      </div>
+      <div className='sidebar-menu-item'>
+        Option
+      </div>
+      <div className='sidebar-menu-item'>
+        Option
+      </div>
+      <div id='sidebar-footer'>
+        Footer
+      </div>
+    </div>
+  </aside>
 
 export default SideBar
--- src/components/root.jsx
+++ src/components/root.jsx
@@ -1,18 +1,15 @@
 import React from 'react'
 import '../styles'
 
-import Home from './Home'
+
 import Header from './Header'
 import SideBar from './SideBar'
+import Content from './Content'
 // import Content from '../routes'
 
-const Root = () => (
-  <div id='stage'>
-    <Header />
-    <Home />
-    {/* <Content /> */}
-    <SideBar />
-  </div>
-)
+const Root = () => [
+  <Content />,
+  <SideBar />
+]
 
 export default Root
